fc93b7c736e9a255f519d44a2e546b93ba423a21
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2024-02-19 17:59:46 -0400

    Created Players Move Animations Configs

--- RetroWaveFuturisticGame/Assets/Scripts/Player/PlayerMoveControls.cs
+++ RetroWaveFuturisticGame/Assets/Scripts/Player/PlayerMoveControls.cs
@@ -9,22 +9,46 @@ public class PlayerMoveControls : MonoBehaviour
 
     private GatherInput gatherInput;
     private Rigidbody2D rb;
+    private Animator anim;
+
+    private int direction = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         gatherInput = GetComponent<GatherInput>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetAnimatorValues();
     }
 
     private void FixedUpdate()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        Flip();
         rb.velocity = new Vector2(speed * gatherInput.valueX, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        if(gatherInput.valueX * direction < 0)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+            direction *= -1;
+        }
+    }
+
+    private void SetAnimatorValues()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
     }
 }
