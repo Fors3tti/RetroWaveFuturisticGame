0adece78c08c7f1720da48d9cc424cba00a8d513
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2024-02-19 20:49:28 -0400

    Added Players Ground Detection

--- RetroWaveFuturisticGame/Assets/Scripts/Player/PlayerMoveControls.cs
+++ RetroWaveFuturisticGame/Assets/Scripts/Player/PlayerMoveControls.cs
@@ -12,6 +12,13 @@ public class PlayerMoveControls : MonoBehaviour
     private Animator anim;
 
     private int direction = 1;
+    private bool grounded = true;
+
+    public float rayLength;
+    public LayerMask groundLayer;
+    public Transform leftPoint;
+    public Transform rightPoint;
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +36,7 @@ public class PlayerMoveControls : MonoBehaviour
 
     private void FixedUpdate()
     {
+        CheckStatus();
         Move();
         Jump();
     }
@@ -43,9 +51,37 @@ public class PlayerMoveControls : MonoBehaviour
     {
         if (gatherInput.jumpInput)
         {
-            rb.velocity = new Vector2(gatherInput.moveInput * speed, jumpForce);
+            if (grounded)
+            {
+                rb.velocity = new Vector2(gatherInput.moveInput * speed, jumpForce);
+            }
         }
         gatherInput.jumpInput = false;
+    }
+
+    private void CheckStatus()
+    {
+        RaycastHit2D leftCheckHit = Physics2D.Raycast(leftPoint.position, Vector2.down, rayLength, groundLayer);
+        RaycastHit2D rightCheckHit = Physics2D.Raycast(rightPoint.position, Vector2.down, rayLength, groundLayer);
+        if (leftCheckHit || rightCheckHit)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+        SeeRays(leftCheckHit, rightCheckHit);
+    }
+
+    private void SeeRays(RaycastHit2D leftCheckHit, RaycastHit2D rightCheckHit)
+    {
+        Color color1 = leftCheckHit ? Color.red : Color.green;
+        Color color2 = rightCheckHit ? Color.red : Color.green;
+
+        Debug.DrawRay(leftPoint.position, Vector2.down * rayLength, color1);
+        Debug.DrawRay(rightPoint.position, Vector2.down * rayLength, color2);
+
     }
 
     private void Flip()
