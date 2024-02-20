9bb8941ab85897bf6c094aeefffb77362d6b35b5
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2024-02-19 22:18:07 -0400

    Created Players Jump Animations Config

--- RetroWaveFuturisticGame/Assets/Scripts/Player/PlayerMoveControls.cs
+++ RetroWaveFuturisticGame/Assets/Scripts/Player/PlayerMoveControls.cs
@@ -96,5 +96,7 @@ public class PlayerMoveControls : MonoBehaviour
     private void SetAnimatorValues()
     {
         anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("JumpSpeed", rb.velocity.y);
+        anim.SetBool("Grounded", grounded);
     }
 }
