64bda07cbfc9986ec5ece2f79c83cbdf7754ecfd
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2024-02-24 13:06:55 -0400

    Created Players Health and Take Damage Configs

--- RetroWaveFuturisticGame/Assets/Scripts/Player/PlayerStats.cs
+++ RetroWaveFuturisticGame/Assets/Scripts/Player/PlayerStats.cs
@@ -7,6 +7,8 @@ public class PlayerStats : MonoBehaviour
     public float maxHealth;
     public float health;
 
+    public bool canTakeDamge = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +18,33 @@ public class PlayerStats : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
-        health -= damage;
-
-        if(health <= 0)
+        if (canTakeDamge)
         {
-            GetComponent<PolygonCollider2D>().enabled = false;
-            GetComponentInParent<GatherInput>().DisableControls();
-            Debug.Log("The player is dead");
+            health -= damage;
+            //play hurt animation
+
+            if (health <= 0)
+            {
+                GetComponent<PolygonCollider2D>().enabled = false;
+                GetComponentInParent<GatherInput>().DisableControls();
+                Debug.Log("The player is dead");
+            }
+
+            StartCoroutine(DamagePrevention());
+        } 
+    }
+
+    private IEnumerator DamagePrevention()
+    {
+        canTakeDamge = false;
+        yield return new WaitForSeconds(0.15f);
+        if(health > 0)
+        {
+            canTakeDamge = true;
+        }
+        else
+        {
+            //play death animation
         }
     }
 }
