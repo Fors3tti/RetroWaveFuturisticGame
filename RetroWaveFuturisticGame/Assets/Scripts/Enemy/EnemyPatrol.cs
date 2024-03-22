11330256ef4d09cf82c5950eef6d389d8dd0e61f
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2024-03-22 07:53:22 -0400

    Created Enemy Patrol Move Configs

--- RetroWaveFuturisticGame/Assets/Scripts/Enemy/EnemyPatrol.cs
+++ RetroWaveFuturisticGame/Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class EnemyPatrol : Enemy
 {
+    public float speed;
+    private int direction = 1;
+
+    public Transform groundCheck;
+    public Transform wallCheck;
+    public LayerMask layerToCheck;
+
+    private bool detectGround;
+    private bool detecWall;
+
+    public float radius;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,12 +24,31 @@ public class EnemyPatrol : Enemy
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(2, rb.velocity.y);
+        Flip();
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Flip()
+    {
+        detectGround = Physics2D.OverlapCircle(groundCheck.position, radius, layerToCheck);
+        detecWall = Physics2D.OverlapCircle(wallCheck.position, radius, layerToCheck);
+
+        if(detecWall || detectGround == false)
+        {
+            direction *= -1;
+            transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, radius);
+        Gizmos.DrawWireSphere(wallCheck.position, radius);
     }
 }
